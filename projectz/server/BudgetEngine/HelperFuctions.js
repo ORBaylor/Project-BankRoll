d7da93ed5711508085f868a6d99d7e2be55dcf4b
Omar B <63411822+ORBaylor@users.noreply.github.com>
2023-09-10 22:17:10 -0400

    First Commit

---
+++ projectz/server/BudgetEngine/HelperFuctions.js
@@ -0,0 +1,33 @@
+
+export function ReturnHalf(){
+
+}
+
+export function ReturnPercentage(amount, dividend){
+
+}
+
+export function SubtractAmountFromDebt(debt, amount){
+
+
+}
+
+export function PayCurrentDebt(debt, amount){
+
+}
+
+export function FindLowestDebt(){
+
+}
+
+export function SortDebtLowToHigh(){
+
+}
+
+export function SortDebtHighToLow(){
+
+}
+
+export function SortDebtCustom(){
+    
+}
