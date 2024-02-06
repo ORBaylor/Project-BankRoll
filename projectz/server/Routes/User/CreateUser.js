2a44d765531ade144b9883e9cafdb3bdea7bbe01
Omar B <63411822+ORBaylor@users.noreply.github.com>
2024-02-05 23:13:36 -0500

    User Controler Complet

--- projectz/server/Routes/User/CreateUser.js
+++ projectz/server/Routes/User/CreateUser.js
@@ -11,6 +11,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const { UserModel, DebtModel, IncomeModel, budgetFrameModel, CustomBudgetFrameModel, BudgetOutcomeModel, CustomBudgetOutcomeModel, debtPayOffTimeFrameModel, CustomDebtModel, customDebtPayOffTimeFrameModel } = require('../../MongoSchema/SchemaModel.js')
 const { SortDebtCustom, CustomPayBareMinimum, UpdateMiniMumPayment, AmountAddedToCurrentDebt, FindAmountLeftOver, FindAllCurrentDebt, PayOffRemainingDebt, CheckIfAllDebtsArePaid, CustomDebtPaymentFrame, getTotalIncomeAmount, GetTotalIntrest, calculatePayoffDate, GetTotalPayments, GetMonthlyIntrestRate, PayBareMinimum, DivideIncomeByOurr, ReturnErrorFrame } = require('../../BudgetEngine/HelperFuctions.js');
 const mongoose = require('mongoose');
+const { json } = require('body-parser');
 
 //const router = express.Router();
 //import { express } from "express";
@@ -35,36 +36,69 @@ const client = new MongoClient(uri, {
 
 
 //Create a user
-router.post('/user/create', async (req, res) => {
+router.post('/create', async (req, res) => {
 
     //What is given to the router
     //req
 
     //What is returned from the router
     //res
-
-    const firstName = req.FirstName;
-    const lastname = req.LastName;
-    const userName = req.UserName;
-    const password = req.Password;
-    const emailAdress = req.ContactInformation.EmailAdress;
-    const phoneNumber = req.ContactInformation.PhoneNumber;
-
-    const newUser = new UserModel({ FirstName: firstName, LastName: lastname, UserName: userName, Password: password, ContactInformation: { EmailAdress: emailAdress, PhoneNumber: phoneNumber } })
-
-    mongoose.createConnection(uri).asPromise(() => {
-
-        // IncomeModel.create({ name: 'guns', amount: 200, occurrence: 'weekly' })
-        console.log("connect");
-        const db = client.db("test");
-
-        const userCollection = db.collection("usermodels");
-
-        const insertedUser = userCollection.insertOne(newUser);
-        //const insertDebt = debtCollection.insertOne(debt);
+    console.log(req)
+    const firstName = req.body.FirstName;
+    const lastname = req.body.LastName;
+    const userName = req.body.UserName;
+    const password = req.body.Password;
+    const emailAdress = req.body.ContactInformation.EmailAdress;
+    //const emailAdress = "TheWeedMan@HotMail.com";
+    console.log(emailAdress)
+    const phoneNumber = req.body.ContactInformation.PhoneNumber;
+    //const phoneNumber = "999-999-9999";
 
 
-    });
+    //const newUser = new UserModel({ FirstName: firstName, LastName: lastname, UserName: userName, Password: password, ContactInformation: { EmailAdress: emailAdress, PhoneNumber: phoneNumber } })
+
+
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        mongoose.connect(uri);
+
+        //await client.connect();
+
+        const usersCollection = client.db('test').collection('usermodels');
+
+        //const userNew = mongoose.model('UserModel', schema);
+        await usersCollection.insertOne({ FirstName: firstName, LastName: lastname, UserName: userName, Password: password, ContactInformation: { EmailAdress: emailAdress, PhoneNumber: phoneNumber } }).then((newUser) => {
+
+            if (!newUser) {
+                return res.status(500).send('Internal Server Error');
+            } else {
+                return res.status(200).send(`User Created: ${newUser.insertedId.toString()}`);
+            }
+        });
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+
+    // mongoose.createConnection(uri).asPromise(() => {
+
+    //     // IncomeModel.create({ name: 'guns', amount: 200, occurrence: 'weekly' })
+    //     console.log("connect");
+    //     const db = client.db("test");
+
+    //     const userCollection = db.collection("usermodels");
+
+    //     const insertedUser = userCollection.insertOne(newUser);
+    //     //const insertDebt = debtCollection.insertOne(debt);
+
+
+    // });
     // await mongoose.connect().then(() => {
 
     //     // IncomeModel.create({ name: 'guns', amount: 200, occurrence: 'weekly' })
@@ -92,21 +126,23 @@ router.post('/user/create', async (req, res) => {
 })
 
 //Update a user
-router.post('/user/update', async (req, res) => {
+router.post('/update', async (req, res) => {
     const db = client.db("test");
     const userCollection = db.collection("usermodels");
     //The id of the user that needs to be updated
     //65a74942971fc53c89aebe1a
-    const userId = req._id;
-    const firstName = req.FirstName;
-    const lastname = req.LastName;
-    const userName = req.UserName;
-    const password = req.Password;
-    const emailAdress = req.ContactInformation.EmailAdress;
-    const phoneNumber = req.ContactInformation.PhoneNumber;
+    const userId = req.body._id;
+    const firstName = req.body.FirstName;
+    const lastname = req.body.LastName;
+    const userName = req.body.UserName;
+    const password = req.body.Password;
+    const emailAdress = req.body.ContactInformation.EmailAdress;
+    //const emailAdress = "TheWeedMan@HotMail.com";
+    console.log(emailAdress)
+    const phoneNumber = req.body.ContactInformation.PhoneNumber;
 
     //A new user model with all of the updates
-    const UserUpdates = new UserModel({ FirstName: firstName, LastName: lastname, UserName: userName, Password: password, ContactInformation: { EmailAdress: emailAdress, PhoneNumber: phoneNumber } })
+
 
     //  const userTo = require('../MongoSchema/SchemaModel.js/UserModel'); // Import your Todo model
 
@@ -117,12 +153,27 @@ router.post('/user/update', async (req, res) => {
     const filter = { _id: new ObjectId(userId) }
 
     try {
-        await userCollection.updateOne(filter, UserUpdates, { new: false })
+
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        mongoose.connect(uri);
+
+        //await client.connect();
+
+        const usersCollection = client.db('test').collection('usermodels');
+
+
+        await userCollection.updateOne(filter, { $set: { FirstName: firstName, LastName: lastname, UserName: userName, Password: password, ContactInformation: { EmailAdress: emailAdress, PhoneNumber: phoneNumber } } }, { new: false })
             .then(updatedDocument => {
                 if (!updatedDocument) {
                     console.log('Document not found');
+                    return res.sendStatus(500);
                 } else {
                     console.log('Updated document:', updatedDocument);
+                    return res.sendStatus(200)
                 }
             })
             .catch(error => {
@@ -146,7 +197,7 @@ router.post('/user/update', async (req, res) => {
 })
 
 //View a user
-router.get('/user/view', async (req, res) => {
+router.post('/view', async (req, res) => {
 
 
     try {
@@ -154,16 +205,17 @@ router.get('/user/view', async (req, res) => {
         const userCollection = db.collection("usermodels");
         mongoose.createConnection(uri);
         console.log("Connected")
-        const filter = { _id: new ObjectId(req._id) }
+        const filter = { _id: new ObjectId(req.body._id) }
 
 
         let returnUser = await userCollection.findOne(filter).then(foundUser => {
             if (!foundUser) {
                 console.log('User not found');
+                return res.sendStatus(400);
 
             } else {
                 console.log('User document:', foundUser);
-                return res.json(foundUser);
+                return res.send(foundUser)
             }
         })
             .catch(error => {
@@ -181,4 +233,39 @@ router.get('/user/view', async (req, res) => {
 
 })
 
+//delete a user
+router.delete('/delete', async (req, res) => {
+
+
+    try {
+        const db = client.db("test");
+        const userCollection = db.collection("usermodels");
+        mongoose.createConnection(uri);
+        console.log("Connected")
+        const filter = { _id: new ObjectId(req.body._id) }
+
+
+        await userCollection.deleteOne(filter).then(foundUser => {
+            if (!foundUser) {
+                console.log('User not found');
+                return res.sendStatus(400);
+
+            } else {
+                console.log('User document:', foundUser);
+                return res.send(foundUser.acknowledged);
+            }
+        })
+            .catch(error => {
+                console.error('Error updating document:', error.message);
+            });
+
+
+
+    } catch (error) {
+
+    }
+
+
+
+})
 module.exports = router;
